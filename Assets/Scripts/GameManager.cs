55969c2b020eb54ab61015345f476d43af1f8817
Enrique Soria <ensomag@inf.upv.es>
2014-10-17 13:03:49 +0200

    Indenting and removing stupid comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,30 +3,21 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public	static	GameManager		instance;
+	public	static GameManager instance;
 	
 	// Game related
 	public	float	gameWidth	= 6,
-					minXScreen,
-					maxXScreen;
+			minXScreen,
+			maxXScreen;
 
 	// Player related
-	public int		lifePoints	= 100;
-	public int		score		= 0;
-	public int		maxScore	= 0;
-	
-	// Combos
-//	public float	comboStopTime			= 2.0f;		// If no kill in... comboStopTime... 
-//														// seconds, combo kills get restarted.
-//	
-//	//public bool	comboFreeze				= false;
-//	public int		comboFreezeNeededKills	= 10;		// How many kills are needed to active combo
-//	public float	comboFreezeDuration		= 5.0f;		// How much will combo last
-//	public float	comboFreezeRate			= 0.5f;		// How much will be speed decreased
+	public int	lifePoints	= 100;
+	public int	score		= 0;
+	public int	maxScore	= 0;
 	
 	// Enemy related
-	public 			float	speedModifier	= 1.0f;				// Enemy speed modifier
-	public const 	float	spawnSpeedMod	= 1.0f;				// Enemy spawn modifier
+	public 		float	speedModifier	= 1.0f;		// Enemy speed modifier
+	public const 	float	spawnSpeedMod	= 1.0f;		// Enemy spawn modifier
 	
 	
 	
@@ -45,8 +36,8 @@ public class GameManager : MonoBehaviour {
 		maxXScreen =  gameWidth / 2.0f;
 		
 		InvokeRepeating("CreateEnemy", 
-						Random.Range(0.2f * spawnSpeedMod, 1.0f * spawnSpeedMod), 
-						Random.Range(0.2f * spawnSpeedMod, 1.0f * spawnSpeedMod)
+				Random.Range(0.2f * spawnSpeedMod, 1.0f * spawnSpeedMod), 
+				Random.Range(0.2f * spawnSpeedMod, 1.0f * spawnSpeedMod)
 		);
 		
 	}
